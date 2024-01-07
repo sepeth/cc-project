1eed1ffd967b3541518ca6c27572ad95cda82d48
Doğan Çeçen <sepeth@gmail.com>
2024-01-07 13:23:27 +0200

    Make clangd work with cmake

--- main.cc
+++ main.cc
@@ -1,3 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Factorials are computed", "[factorial]") { REQUIRE(2 == 2); }
+TEST_CASE("2 equals to 2", "[tag]") { REQUIRE(2 == 2); }
+
+TEST_CASE("4 divided by 2 is 2") { REQUIRE((4 / 2) == 2); }
